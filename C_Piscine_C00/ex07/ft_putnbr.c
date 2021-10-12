af9994812d672fc782bc084c1ac3307e4a9bf2d4
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-12 11:39:44 +0900

    C_Piscine_C00

---
+++ C_Piscine_C00/ex07/ft_putnbr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 11:09:15 by sejeon            #+#    #+#             */
+/*   Updated: 2021/10/12 11:37:40 by sejeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	recursive_f(int nb)
+{
+	if (nb <= 0)
+		return ;
+	recursive_f(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = nb * -1;
+	}
+	recursive_f(nb);
+}			
