1c4694d425d7af86a0317f62de66b3f6231a613c
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-14 23:29:25 +0900

    C_Piscine_C01

--- C_Piscine_C01/ex08/ft_sort_int_tab.c
+++ C_Piscine_C01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 18:41:38 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/12 19:21:42 by sejeon           ###   ########.fr       */
+/*   Updated: 2021/10/14 23:29:04 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	ft_sort_int_tab(int *tab, int size)
 		while (j < size - 1)
 		{
 			if (tab[j] > tab[j + 1])
-				ft_rev_int_tab(tab, j, j+1);
+				ft_rev_int_tab(tab, j, j + 1);
 			j++;
 		}
 		i++;
