8a955552e960f16e39f66c2c2cb025bc4d642f60
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-15 01:11:07 +0900

    C_Piscine_C04

---
+++ C_Piscine_C04/ex01/ft_putstr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 00:53:14 by sejeon            #+#    #+#             */
+/*   Updated: 2021/10/15 00:59:41 by sejeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	strlen;
+
+	strlen = -1;
+	while (str[++strlen] != '\0')
+		ft_putchar(str[strlen]);
+}
---
+++ C_Piscine_C04/ex02/ft_putnbr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 01:00:17 by sejeon            #+#    #+#             */
+/*   Updated: 2021/10/15 01:10:48 by sejeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	recursive_func(int nb)
+{
+	if (nb <= 0)
+		return ;
+	recursive_func(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	int	num;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		if (nb == -2147483648)
+		{
+			num = 214748364;
+			recursive_func(num);
+			ft_putchar(8 + '0');
+		}
+		nb *= 1;
+	}
+	recursive_func(nb);
+}
