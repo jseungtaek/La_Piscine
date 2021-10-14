3a5dadd09008776fe3bd25c2ff6eb320a86af5d0
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-15 00:44:49 +0900

    C_Piscine_C03

---
+++ C_Piscine_C03/ex05/ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/14 23:31:50 by sejeon            #+#    #+#             */
+/*   Updated: 2021/10/15 00:44:23 by sejeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	str_len;
+
+	str_len = 0;
+	while (str[str_len])
+		str_len++;
+	return (str_len);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	while (src[i] != '\0' && dest_len + i + 1 < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	if (dest_len < size)
+		dest[i + dest_len] = '\0';
+	return (dest_len + src_len);
+}
