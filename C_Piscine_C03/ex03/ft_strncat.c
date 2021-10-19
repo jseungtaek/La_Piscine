309fef7226039bcb2297fe8ec377ab1ec46bd61a
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-19 13:45:09 +0900

    C_Piscine_C03

--- C_Piscine_C03/ex03/ft_strncat.c
+++ C_Piscine_C03/ex03/ft_strncat.c
@@ -6,29 +6,21 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 18:30:27 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/19 12:16:14 by sejeon           ###   ########.fr       */
+/*   Updated: 2021/10/19 13:44:53 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-int	ft_strlen(char *str)
-{
-	int	strlen;
-
-	strlen = 0;
-	while (str[strlen])
-		strlen++;
-	return (strlen);
-}
-
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int				dest_len;
+	unsigned int	dest_len;
 	unsigned int	i;
 
-	dest_len = ft_strlen(dest);
+	dest_len = 0;
 	i = 0;
+	while (dest[dest_len])
+		dest_len++;
 	while (i < nb && src[i] != '\0')
 	{
 		dest[dest_len] = src[i];
