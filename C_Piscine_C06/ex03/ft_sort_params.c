87a3914b66d3b5a055830c17bae09136826da878
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-20 01:27:21 +0900

    C_Piscine_C06

--- C_Piscine_C06/ex03/ft_sort_params.c
+++ C_Piscine_C06/ex03/ft_sort_params.c
@@ -1,16 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 23:51:30 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/20 00:51:07 by sejeon           ###   ########.fr       */
+/*   Created: 2021/10/20 00:52:37 by sejeon            #+#    #+#             */
+/*   Updated: 2021/10/20 01:27:02 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (*s1 && *s2 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
 
 void	ft_putstr(char *str)
 {
@@ -27,10 +40,28 @@ void	ft_putstr(char *str)
 
 int	main(int argc, char **argv)
 {
-	while (argc > 1)
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc)
 	{
-		ft_putstr(argv[argc - 1]);
-		argc--;
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
 	}
+	i = 0;
+	while (++i < argc)
+		ft_putstr(argv[i]);
 	return (0);
 }
