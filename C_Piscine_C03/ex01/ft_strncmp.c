49e8bd814788c0d15b7ec06678fe246646d4b533
Seungtaek Jeon <sejeon@c4r2s4.42seoul.kr>
2021-10-20 20:22:12 +0900

    C_Piscine_C03

--- C_Piscine_C03/ex01/ft_strncmp.c
+++ C_Piscine_C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 18:15:28 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/19 13:24:07 by sejeon           ###   ########.fr       */
+/*   Updated: 2021/10/20 20:21:52 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && (*s1 || *s2))
+	while (i < n && *s1 && *s2)
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
