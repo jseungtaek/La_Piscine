84d1447bf1c6f1e939016d6c563870d3f9aa61e2
Seungtaek Jeon <sejeon@c4r5s2.42seoul.kr>
2021-10-22 01:54:40 +0900

    C_Piscine_C04

--- C_Piscine_C04/ex03/ft_atoi.c
+++ C_Piscine_C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 20:42:11 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/21 13:59:14 by sejeon           ###   ########.fr       */
+/*   Updated: 2021/10/22 01:54:15 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	ft_isdigit(char *str, int i)
 	return (0);
 }
 
-void	ft_skip(char *str, int *start)
+int	ft_skip(char *str, int *start)
 {
 	int	i;
 
 	i = 1;
 	while ((str[*start] >= 9 && str[*start] <= 13) || str[*start] == ' ')
-	       *start += 1;
+		*start += 1;
 	while (str[*start] == '+' || str[*start] == '-')
 	{
 		if (str[*start] == '-')
@@ -34,7 +34,7 @@ void	ft_skip(char *str, int *start)
 	}
 	return (i);
 }
-			
+
 int	ft_atoi(char *str)
 {
 	int	i;
@@ -44,7 +44,7 @@ int	ft_atoi(char *str)
 	i = 0;
 	operator = 1;
 	num = 0;
-	opertator = ft_skip(str, &i);
+	operator = ft_skip(str, &i);
 	while (ft_isdigit(str, i))
 	{
 		num = num * 10 + str[i] - '0';
