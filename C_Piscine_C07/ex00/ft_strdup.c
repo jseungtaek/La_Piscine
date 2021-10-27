f5a3fc53ebf4a9c5dd82a8acb48e5120cee931ca
Seungtaek Jeon <sejeon@c2r8s4.42seoul.kr>
2021-10-27 18:06:48 +0900

    C_Piscine_C07

--- C_Piscine_C07/ex00/ft_strdup.c
+++ C_Piscine_C07/ex00/ft_strdup.c
@@ -6,11 +6,21 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 15:21:43 by sejeon            #+#    #+#             */
-/*   Updated: 2021/10/27 16:05:04 by sejeon           ###   ########.fr       */
+/*   Updated: 2021/10/27 18:05:18 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		++len;
+	return (len);
+}
 
 char	*ft_strdup(char *src)
 {
@@ -18,7 +28,7 @@ char	*ft_strdup(char *src)
 	int		i;
 	char	*dest;
 
-	src_len = 0;
+	src_len = ft_strlen(src);
 	i = 0;
 	dest = (char *)malloc(sizeof(char) * src_len + 1);
 	if (dest == 0)
